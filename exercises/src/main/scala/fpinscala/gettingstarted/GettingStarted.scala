d440b0ab2eb53e8a38bb0d9d18b0c3c1e9234a34
Mauricio Jost <mauricio.jost@gmail.com>
2017-02-06 09:02:46 +0100

    Implement currying

--- exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
+++ exercises/src/main/scala/fpinscala/gettingstarted/GettingStarted.scala
@@ -198,7 +198,8 @@ object PolymorphicFunctions {
   // Note that `=>` associates to the right, so we could
   // write the return type as `A => B => C`
   def curry[A,B,C](f: (A, B) => C): A => (B => C) =
-    ???
+    (a: A) => f(a, _)
+
 
   // NB: The `Function2` trait has a `curried` method already
 
