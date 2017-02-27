fb2bb8ba83571caee4f5d8fa5c3ddbbb4b73d755
Mauricio Jost <mauricio.jost@gmail.com>
2017-02-27 09:13:22 +0100

    Implement List.foldLeft (tail recursive)

--- exercises/src/main/scala/fpinscala/datastructures/List.scala
+++ exercises/src/main/scala/fpinscala/datastructures/List.scala
@@ -93,7 +93,13 @@ object List { // `List` companion object. Contains functions for creating and wo
     foldRight(l, 0)((a: A, b: Int) => 1 + b)
   }
 
-  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = ???
+  @annotation.tailrec
+  def foldLeft[A,B](l: List[A], z: B)(f: (B, A) => B): B = {
+    l match {
+      case Nil => z
+      case Cons(x, xs) => foldLeft(xs, f(z, x))(f)
+    }
+  }
 
   def map[A,B](l: List[A])(f: A => B): List[B] = ???
 }
