5e94d862aec5968e3a308c20cd675dfdbecfc11d
Mauricio Jost <mauricio.jost@gmail.com>
2017-12-04 09:11:29 +0100

    Exercises of State

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -17,6 +17,10 @@ object RNG {
     }
   }
 
+  case class Same(seed: Long) extends RNG {
+    def nextInt: (Int, RNG) = (seed.toInt, Same(seed))
+  }
+
   type Rand[+A] = RNG => (A, RNG)
 
   val int: Rand[Int] = _.nextInt
@@ -30,23 +34,55 @@ object RNG {
       (f(a), rng2)
     }
 
-  def nonNegativeInt(rng: RNG): (Int, RNG) = ???
+  def nonNegativeInt(rng: RNG): (Int, RNG) = {
+    val (newInt, newRng) = rng.nextInt
+    val DefaultIntValue = Int.MaxValue
+    val safeNewInt = if (newInt == Int.MinValue) {
+      DefaultIntValue // in case Int.MinValue, which is negative and has no positive counter-part
+    } else {
+      newInt
+    }
 
-  def double(rng: RNG): (Double, RNG) = ???
+    (Math.abs(safeNewInt), newRng)
+  }
+
+  def double(rng: RNG): (Double, RNG) = {
+    val (newPosInt, newRng) = nonNegativeInt(rng)
+    (newPosInt.toDouble / Int.MaxValue, newRng)
+  }
 
   def intDouble(rng: RNG): ((Int,Double), RNG) = ???
 
   def doubleInt(rng: RNG): ((Double,Int), RNG) = ???
 
-  def double3(rng: RNG): ((Double,Double,Double), RNG) = ???
+  def double3(rng0: RNG): ((Double, Double, Double), RNG) = {
+    val (double1, rng1) = double(rng0)
+    val (double2, rng2) = double(rng1)
+    val (double3, rng3) = double(rng2)
+    ((double1, double2, double3), rng3)
+  }
 
-  def ints(count: Int)(rng: RNG): (List[Int], RNG) = ???
 
-  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = ???
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+    (0 until count).foldRight[(List[Int], RNG)]((Nil, rng)) { case (x, (l, r)) => (r.nextInt._1 :: l, r.nextInt._2) }
+  }
 
-  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = ???
+  def map2[A, B, C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = { rng0 =>
+    val (valueA, rng1) = ra(rng0)
+    val (valueB, rng2) = rb(rng1)
+    (f(valueA, valueB), rng2)
+  }
 
-  def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = ???
+  def both[A, B](ra: Rand[A], rb: Rand[B]): Rand[(A, B)] = map2[A, B, (A, B)](ra, rb)((_, _))
+
+  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = fs.foldRight[Rand[List[A]]](unit[List[A]](Nil))((x, xs) => map2(x, xs)(_ :: _))
+
+  def flatMap[A, B](f: Rand[A])(g: A => Rand[B]): Rand[B] = { rng0 =>
+    val (value0, rng1) = f(rng0)
+    val (value1, rng2) = g(value0)(rng1)
+    (value1, rng2)
+  }
+
 }
 
 case class State[S,+A](run: S => (A, S)) {
